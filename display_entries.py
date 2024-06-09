850b8a8bc6903f6639cb3b0b9065693ed78b546c
yannicklimmer <yannick.limmer@icloud.com>
2024-06-09 11:51:14 +0200

    Add results to schedule and display.

--- display_entries.py
+++ display_entries.py
@@ -13,14 +13,17 @@ def display_entries():
                 load_data(d).reset_index(),
                 schedule[schedule.Datetime.dt.date.apply(lambda s: s.strftime('%d-%b')) == d],
                 on=["TeamA", "TeamB"],
-            ).set_index(["Datetime"] + INDEX_COLUMNS) for d in dates
+            ).set_index(["Datetime", "ResultA", "ResultB"] + INDEX_COLUMNS) for d in dates
         ]
         df = pd.concat(dfs, axis=0)
         df = df.loc[(df.reset_index().Datetime < get_now()).values, :]
         df = df.rename({"ScoreA": "A", "ScoreB": "B", "Factor": "X"}, axis=1)
-        df = df.unstack('Name').fillna(0)
+        df = df.unstack('Name')
+        for col in df.columns:
+            if "X" in col:
+                df[col] = df[col].fillna(0)
         df.columns = df.columns.swaplevel(0, 1)
-        df = df.sort_index(axis=1, level=0).astype(int)
+        df = df.sort_index(axis=1, level=0)  # .astype(int)
         df.columns = [' '.join(col).strip() for col in df.columns.values]
         st.dataframe(df, column_config={
                 k: st.column_config.ProgressColumn(
@@ -33,5 +36,9 @@ def display_entries():
                 ) for k in df.columns if "X" in k
             } | {
                 "Datetime": st.column_config.DateColumn("Date", format="DD-MMM, HH:mm")
+            } | {
+                "TeamB": st.column_config.TextColumn("B")
+            } | {
+                "TeamA": st.column_config.TextColumn("A")
             },
         )
